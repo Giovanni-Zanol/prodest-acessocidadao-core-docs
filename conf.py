d417c2181befa7ed01a2fa5135c02d9c011ff8d6
Tadeu <marcos.torres@prodest.es.gov.br>
2019-05-02 17:05:59 -0300

    change de encoding

--- conf.py
+++ conf.py
@@ -51,7 +51,7 @@ language = 'pt-br'
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-source_encoding = 'iso-8859-15'
+#source_encoding = 'iso-8859-15'
 
 # -- Options for HTML output -------------------------------------------------
 
